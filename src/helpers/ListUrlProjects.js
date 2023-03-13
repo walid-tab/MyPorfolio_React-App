bfcc1251cd0c30aa03225a337957daa169212306
Walid <walid.tabbabi7@gmail.com>
2023-03-13 01:29:16 +0100

    second commit

--- src/helpers/ListUrlProjects.js
+++ src/helpers/ListUrlProjects.js
@@ -1,20 +1,9 @@
-export const ListUrl =[
-    {
-        link1:"https://github.com/walid-tab/Search-Job-App"
-    },
-    {
-        link2:"https://github.com/walid-tab/Movie-App-React"
-    },
-    {
-        link3:"https://github.com/walid-tab/CRUD-User-React-app-with-Firebase-"
-    },
-    {
-        link4:"https://github.com/walid-tab/todoList_react_app_using_Typescript"
-    },
-    {
-        link5:"https://github.com/walid-tab/Quiz-App-React"
-    },
-    {
-        link6:"https://github.com/walid-tab/MERN-App-CRUD"
-    }
-]
+export const postUrl1 = "https://github.com/walid-tab/Search-Job-App";
+export const postUrl2 = "https://github.com/walid-tab/Movie-App-React";
+export const postUrl3 = "https://github.com/walid-tab/CRUD-User-React-app-with-Firebase-";
+export const postUrl4 = "https://github.com/walid-tab/todoList_react_app_using_Typescript";
+export const postUrl5 = "https://github.com/walid-tab/Quiz-App-React"
+export const postUrl6 = "https://github.com/walid-tab/MERN-App-CRUD";
+
+
+
--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -4,43 +4,48 @@ import Proj3 from "../assets/Proj3.png";
 import Proj4 from "../assets/Proj4.png";
 import Proj5 from "../assets/Proj5.png";
 import Proj6 from "../assets/Proj6.png";
-import { ListUrl } from "./ListUrlProjects";
+import { postUrl1 } from "./ListUrlProjects";
+import { postUrl2 } from "./ListUrlProjects";
+import { postUrl3 } from "./ListUrlProjects";
+import { postUrl4 } from "./ListUrlProjects";
+import { postUrl5 } from "./ListUrlProjects";
+import { postUrl6 } from "./ListUrlProjects";
 
 export const ProjectList = [
   {
     name: "Search Job Web App",
     image: Proj1,
     skills: "React.js, Express.js, Node.js, MongoDB",
-    url: ListUrl.link1,
+    url: postUrl1,
   },
   {
     name: "Movie Web App",
     image: Proj2,
     skills: "React.js, bootstrap ",
-    url: ListUrl.link2,
+    url: postUrl2,
   },
   {
     name: "User App with Firebase",
     image: Proj3,
     skills: "React.js, Firebase, bootstrap",
-    url: ListUrl.link3,
+    url: postUrl3,
   },
   {
     name: "TodoList App with React-TypeScript",
     image: Proj4,
     skills: "React, Typescript, Bootstrap",
-    url: ListUrl.link4,
+    url: postUrl4,
   },
   {
     name: "Quiz App",
     image: Proj5,
     skills: "React.js, bootstrap",
-    url: ListUrl.link5,
+    url: postUrl5,
   },
   {
     name: "Contact List MERN App",
     image: Proj6,
     skills: "React.js, Node.js, Express.js, MongoDB, Redux, axios",
-    url: ListUrl.link6,
+    url: postUrl6,
   },
 ];
